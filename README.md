6982f60aa7e967ffc549535e38c0ae547d69da9f
Alexander Matson <alexmat2on@protonmail.com>
2018-03-14 19:41:10 -0400

    gen macros

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
 # Pictualize
 
-DB Project, made in Flask.
+CS 336 DB Project, made in [Flask](https://code.tutsplus.com/tutorials/creating-a-web-app-from-scratch-using-python-flask-and-mysql--cms-22972).
 
-[Simple Flask Tutorial](https://code.tutsplus.com/tutorials/creating-a-web-app-from-scratch-using-python-flask-and-mysql--cms-22972)
+## Accomplished
+[x] Began simple web app with Flask (in `FlaskApp/`) that establishes a DB connection
+[x] Write SQL statements to generate all 8 schemas (in `sql/`)
+[x] Write a helper script in Python to auto-generate memes for future testing (in `helper-tools/`)
+
+## To-Do
+[ ] Create appropriate table constraints for tables
+[ ] Connect sign-up form to MySQL backend
+[ ] Write helper script to auto-generate fake users with fake posts
+[ ] Create homescreen UI displaying other user's posts
+[ ] Allow user to save other user's templates/images to personal library
+[ ] Allow user to create new posts from image template and post
 
 ## Running
-Make sure you've installed flask with `pip install flask`, go into the FlaskApp directory and execute `python app.py`. 
+* Install flask with `pip install flask`
+* Install flask-mysql with `pip install flask-mysql`
+* Go into the FlaskApp directory and execute `python app.py`.
 
-The app will run by default on `localhost:5000`
+The app connects to a local MySQL instance, which must contain a database called `Pictualize`,
+and will run by default on `localhost:5000`
---
+++ helper-tools/gen-macros/gen-macros.py
@@ -0,0 +1,65 @@
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+
+from math import ceil
+import os
+import random
+
+# Read all possible top and bottom texts ---------------------------------------
+with open('top.txt') as t:
+    top_options = t.readlines()
+top_options = [x.strip() for x in top_options]
+
+with open('bot.txt') as b:
+    bot_options = b.readlines()
+bot_options = [x.strip() for x in bot_options]
+
+# Function to superimpose text on an image -------------------------------------
+def drawText(img, textstr, position):
+    # Make font size 7% of the image width
+    font = ImageFont.truetype("impact.ttf", ceil(img.size[0] * 0.07))
+
+    text_x = font.getsize(textstr)[0]
+    text_y = font.getsize(textstr)[1]
+    x = (img.size[0] - text_x) / 2
+
+    if position == "TOP":
+        y = 5
+    elif position == "BOT":
+        y = img.size[1] - text_y - 10
+
+    if text_x >= img.size[0]:
+        sp = textstr.split()
+        words = len(sp)
+        topline = ""
+        nextline = ""
+        for i in range(0, words // 2):
+            topline += sp[i] + " "
+
+        for i in range(words // 2, words):
+            nextline += sp[i] + " "
+
+        nextx = (img.size[0] - font.getsize(nextline)[0]) / 2
+        draw.text((nextx + 2, y + text_y + 8), nextline, (0, 0, 0), font=font)
+        draw.text((nextx, y + text_y + 5), nextline, (255, 255, 255), font=font)
+
+        textstr = topline
+        x = (img.size[0] - font.getsize(textstr)[0]) / 2
+
+    draw.text((x + 2, y + 2), textstr, (0, 0, 0), font=font)
+    draw.text((x, y), textstr, (255, 255, 255), font=font)
+
+count = 0
+for filename in os.listdir("base_imgs"):
+    img = Image.open("base_imgs/" + filename)
+    draw = ImageDraw.Draw(img)
+
+    text_top = random.choice(top_options)
+    text_bot = random.choice(bot_options)
+
+    drawText(img, text_top, "TOP")
+    drawText(img, text_bot, "BOT")
+
+    img.save('random_memes/wow' + str(count) + '.jpg')
+    count += 1
