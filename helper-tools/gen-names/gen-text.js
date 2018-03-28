3d4ffa6e86f2aea47a44739a0de7a6d542b4f4e5
Alexander Matson <alexmat2on@protonmail.com>
2018-03-28 19:04:22 -0400

    gen tools

---
+++ helper-tools/gen-names/gen-text.js
@@ -0,0 +1,47 @@
+var faker = require('faker');
+var mysql = require('mysql');
+var fs = require('fs');
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'root',
+	database: 'Pictualize'
+});
+
+connection.connect();
+
+for (var x = 0; x < 25; x++) {
+	var top = faker.lorem.word();
+
+  console.log(top);
+  fs.writeFile("topMeme.txt", top, function (err) {
+    if (err) throw err;
+  })
+	// Insert user data into Users table
+	// var insertUserSQL = 'INSERT INTO Users VALUES(';
+	// insertUserSQL += "'" + un + "',";
+	// insertUserSQL += "'" + fn + "',";
+	// insertUserSQL += "'" + ln + "',";
+	// insertUserSQL += "'" + em + "'";
+	// insertUserSQL += ")";
+  //
+	// connection.query(insertUserSQL, function (err, result) {
+	// 	if (err) throw err;
+	// 	console.log("inserted", insertUserSQL);
+	// });
+  //
+	// // Give the user an Avatar
+	// var insertAvatarSQL = 'INSERT INTO Profiles VALUES(';
+	// insertAvatarSQL += "'" + un + "',";
+	// insertAvatarSQL += "'" + av + "'";
+	// insertAvatarSQL += ")";
+  //
+	// console.log(insertAvatarSQL);
+	// connection.query(insertAvatarSQL, function (err, result) {
+	// 	if (err) throw err;
+	// 	console.log("inserted", insertAvatarSQL);
+	// });
+}
+
+connection.end();
--- helper-tools/gen-names/gen-users.js
+++ helper-tools/gen-names/gen-users.js
@@ -10,26 +10,37 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-var un = faker.internet.userName();
-var fn = faker.name.firstName();
-var ln = faker.name.lastName();
-var em = faker.internet.email();
+for (var x = 0; x < 50; x++) {
+	var un = faker.internet.userName();
+	var fn = faker.name.firstName();
+	var ln = faker.name.lastName();
+	var em = faker.internet.email();
+	var av = faker.image.avatar();
 
-console.log(fn);
-console.log(ln);
-console.log(un);
-console.log(em);
+	// Insert user data into Users table
+	var insertUserSQL = 'INSERT INTO Users VALUES(';
+	insertUserSQL += "'" + un + "',";
+	insertUserSQL += "'" + fn + "',";
+	insertUserSQL += "'" + ln + "',";
+	insertUserSQL += "'" + em + "'";
+	insertUserSQL += ")";
 
-var insertUserSQL = 'INSERT INTO Users VALUES(' +
-											"'" + un + ',' + fn + ',' + ln + ','+ em + ')';
-connection.query(insertUserSQL, function (err, result) {
-	if (err) throw err;
-	console.log("inserted", un, fn, ln, em);
-})
+	connection.query(insertUserSQL, function (err, result) {
+		if (err) throw err;
+		console.log("inserted", insertUserSQL);
+	});
 
-connection.query('SELECT * FROM Users', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results, fields);
-});
+	// Give the user an Avatar
+	var insertAvatarSQL = 'INSERT INTO Profiles VALUES(';
+	insertAvatarSQL += "'" + un + "',";
+	insertAvatarSQL += "'" + av + "'";
+	insertAvatarSQL += ")";
+
+	console.log(insertAvatarSQL);
+	connection.query(insertAvatarSQL, function (err, result) {
+		if (err) throw err;
+		console.log("inserted", insertAvatarSQL);
+	});
+}
 
 connection.end();
