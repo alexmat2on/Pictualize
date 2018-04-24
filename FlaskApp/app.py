68da5d272011c4b57bd199baa9b9e47c8df25b4c
Alexander Matson <alexmat2on@protonmail.com>
2018-04-24 10:58:20 -0400

    Use Flask Sessions to keep track of user's logged in status.

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, json, request, redirect
+from flask import escape, Flask, json, redirect, render_template, request, session, url_for
+
 from flask.ext.mysql import MySQL
 
 app = Flask(__name__)
@@ -15,55 +16,61 @@ mysql.init_app(app)
 conn = mysql.connect()
 cursor=conn.cursor()
 
-print(conn)
-
-
-
+# INDEX PAGE
 @app.route("/")
 def main():
-    return render_template('index.html')
-
-@app.route("/home")
-def home():
-    links = ["http://lorempixel.com/640/480/food", "http://lorempixel.com/640/480/abstract", "http://lorempixel.com/640/480/sports", "http://lorempixel.com/640/480/car"]
-    return render_template('homepage.html', picLinks = links)
-
+    # Check if a user is logged in and show the respective page
+    if 'username' in session:
+        links = ["http://lorempixel.com/640/480/food", "http://lorempixel.com/640/480/abstract", "http://lorempixel.com/640/480/sports", "http://lorempixel.com/640/480/car"]
+        return render_template('homepage.html', picLinks = links)
+    else:
+        return render_template('index.html')
 
 @app.route("/signup", methods=['POST'])
 def signup():
+    # Store data from the form fields
     _firstname = request.form['createFirstName']
     _lastname = request.form['createLastName']
     _email = request.form['createEmail']
     _username = request.form['createUsername']
 
+    # Insert the data into the Users table via a Stored Procedure
     cursor.callproc('createUser',(_firstname,_lastname,_email,_username))
-
     data = cursor.fetchall()
     if len(data) is 0:
         conn.commit()
-        return redirect("/home")
-        #return json.dumps({'message':'User created successfully !'}), 200
+        return redirect("/")
     else:
-        return json.dumps({'errorrrr': str(data[0])})
+        return json.dumps({'error': str(data[0])})
 
-@app.route("/login", methods=['POST'])
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    # _username = "Aidan.Marks"
     _username = request.form['inputUsername']
 
+    # Verify the username is in the database
     cursor.execute("SELECT * FROM Users where userID=" + "'" + _username + "'")
     data = cursor.fetchone()
     if data is None:
         return "Invalid username"
     else:
+        # When the username is in database, create a new session (cookie)
+        #      for authenticating it on other pages
+        if request.method == 'POST':
+            session['username'] = _username;
+            return redirect("/")
 
-        return redirect("/home")
+@app.route("/logout")
+def logout():
+    # Delete the user's session and return them to homepage
+    session.pop('username', None)
+    return redirect("/")
 
 @app.route("/upload")
 def upload():
     return render_template('upload.html')
 
+# Secret key to encrypt session cookies
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT';
 
-        
 if __name__ == "__main__":
     app.run(debug=True)
